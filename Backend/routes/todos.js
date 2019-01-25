5b95f70744d2574baff1e2782e0cb2a9a4d02263
nanoDW <s.fluder@interia.eu>
2019-01-25 10:55:44 +0100

    Repaired res with empty array of users and todos

--- Backend/routes/todos.js
+++ Backend/routes/todos.js
@@ -1,4 +1,4 @@
-const auth = require('../middleware/auth');
+const auth = require("../middleware/auth");
 const express = require("express");
 const mongoDebugger = require("debug")("app:db");
 const validate = require("../app/models/validate");
@@ -8,99 +8,96 @@ const router = express.Router();
 
 const mongoose = require("mongoose");
 mongoose
-    .connect("mongodb://localhost/todoapp")
-    .then(() => mongoDebugger("Connected to the MongoDB"))
-    .catch(err => mongoDebugger("Could not connect to the database", err));
+  .connect("mongodb://localhost/todoapp")
+  .then(() => mongoDebugger("Connected to the MongoDB"))
+  .catch(err => mongoDebugger("Could not connect to the database", err));
 
 router.post("/", auth, async (req, res) => {
-    const {
-        error
-    } = validate.validateTodo(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+  const { error } = validate.validateTodo(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    const todo = new Schemas.Todo({
-        nick: req.body.nick,
-        description: req.body.description,
-        priority: req.body.priority,
-        days: req.body.days,
-        done: req.body.done,
-        num: req.body.num
-    });
-    await todo.save();
-    res.send(todo)
+  const todo = new Schemas.Todo({
+    nick: req.body.nick,
+    description: req.body.description,
+    priority: req.body.priority,
+    days: req.body.days,
+    done: req.body.done,
+    num: req.body.num
+  });
+  await todo.save();
+  res.send(todo);
 });
 
 router.get("/me", auth, async (req, res) => {
-    const todos = await Schemas.Todo.find({
-            $and: [{
-                nick: req.params.nick
-            }, {
-                done: false
-            }]
-        })
-        .sort({
-            date: 1
-        })
-        .select('_id description done priority');
-    if (!todos) return res.status(404).send("You have no todos yet");
-    res.send(todos);
+  const user = await Schemas.User.findById(req.user._id).select("nick");
+  const todos = await Schemas.Todo.find({
+    nick: user.nick
+  });
+  if (!todos[0]) return res.status(404).send("You have no todos yet");
+  res.send(todos);
 });
 
 router.get("/:nick/done", auth, async (req, res) => {
-    const todos = await Schemas.Todo.find({
-            $and: [{
-                nick: req.params.nick
-            }, {
-                done: true
-            }]
-        })
-        .sort({
-            date: 1
-        })
-        .select('_id description done priority');
-    if (!todos) return res.status(404).send("You have no todos yet");
-    res.send(todos);
+  const user = await Schemas.User.findById(req.user._id).select("nick");
+  const todos = await Schemas.Todo.find({
+    $and: [
+      {
+        nick: user.nick
+      },
+      {
+        done: true
+      }
+    ]
+  })
+    .sort({
+      date: 1
+    })
+    .select("_id description done priority");
+  if (!todos[0]) return res.status(404).send("You have no todos yet");
+  res.send(todos);
 });
 
-
 router.get("/todos/:id", auth, async (req, res) => {
-    const todo = await Schemas.Todo.findById(req.params.id);
-    if (!todo) return res.status(404).send("This todo doesnt exist.");
-    res.send(todo);
+  const todo = await Schemas.Todo.findById(req.params.id);
+  if (!todo) return res.status(404).send("This todo doesnt exist.");
+  res.send(todo);
 });
 
 router.put("/todos/:id", auth, async (req, res) => {
-    const {
-        error
-    } = validate.validateTodoEdited(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-    await Schemas.Todo.updateOne({
-        _id: req.params.id
-    }, {
-        $set: {
-            priority: req.body.priority
-        }
-    });
-    res.send("Done!");
+  const { error } = validate.validateTodoEdited(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  await Schemas.Todo.updateOne(
+    {
+      _id: req.params.id
+    },
+    {
+      $set: {
+        priority: req.body.priority
+      }
+    }
+  );
+  res.send("Done!");
 });
 
 router.put("/todos/:id/done", auth, async (req, res) => {
-    await Schemas.Todo.updateOne({
-        _id: req.params.id
-    }, {
-        $set: {
-            done: true
-        }
-    });
-    res.send("Congrats!");
+  await Schemas.Todo.updateOne(
+    {
+      _id: req.params.id
+    },
+    {
+      $set: {
+        done: true
+      }
+    }
+  );
+  res.send("Congrats!");
 });
 
 router.delete("/:id", auth, (req, res) => {
-    const todo = req.params.id;
-    Todo.deleteTodo(todo, cb => {
-        res.send(JSON.stringify(cb));
-    })
-
+  const todo = req.params.id;
+  Todo.deleteTodo(todo, cb => {
+    res.send(JSON.stringify(cb));
+  });
 });
 
 module.exports = router;
--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -1,10 +1,10 @@
-const asyncMiddleware = require('../middleware/asyncMiddleware');
-const error = require('../middleware/error');
-const auth = require('../middleware/auth');
-const jwt = require('jsonwebtoken');
-const config = require('config');
-const bcrypt = require('bcryptjs');
-let _ = require('lodash');
+const asyncMiddleware = require("../middleware/asyncMiddleware");
+const error = require("../middleware/error");
+const auth = require("../middleware/auth");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const bcrypt = require("bcryptjs");
+let _ = require("lodash");
 const express = require("express");
 const mongoDebugger = require("debug")("app:db");
 const validate = require("../app/models/validate");
@@ -14,81 +14,120 @@ const router = express.Router();
 
 const mongoose = require("mongoose");
 mongoose
-    .connect("mongodb://localhost/todoapp")
-    .then(() => mongoDebugger("Connected to the MongoDB"))
-    .catch(err => mongoDebugger("Could not connect to the database", err));
+  .connect("mongodb://localhost/todoapp")
+  .then(() => mongoDebugger("Connected to the MongoDB"))
+  .catch(err => mongoDebugger("Could not connect to the database", err));
 
-router.get("/", auth, asyncMiddleware(async (req, res) => {
-    const users = await Schemas.User.find().select('nick last');
+router.get(
+  "/",
+  auth,
+  asyncMiddleware(async (req, res) => {
+    const users = await Schemas.User.find().select("nick last");
     res.send(users);
-}));
+  })
+);
 
-router.post("/", asyncMiddleware(async (req, res) => {
-    const {
-        error
-    } = validate.validateUser(req.body);
+router.post(
+  "/",
+  asyncMiddleware(async (req, res) => {
+    const { error } = validate.validateUser(req.body);
     if (error) return res.status(400).send(error.details[0].message);
     let user = await Schemas.User.findOne({
-        $or: [{
-            nick: req.body.nick
-        }, {
-            email: req.body.email
-        }]
+      $or: [
+        {
+          nick: req.body.nick
+        },
+        {
+          email: req.body.email
+        }
+      ]
     });
-    if (user) return res.status(400).send('User already exist');
+    if (user) return res.status(400).send("User already exist");
     user = new Schemas.User({
-        nick: req.body.nick,
-        password: req.body.password,
-        email: req.body.email,
-        last: 0
+      nick: req.body.nick,
+      password: req.body.password,
+      email: req.body.email,
+      last: 0
     });
     const salt = await bcrypt.genSalt();
     user.password = await bcrypt.hash(user.password, salt);
     await user.save();
-    const token = jwt.sign({
+    const token = jwt.sign(
+      {
         _id: user._id
-    }, config.get('jwtPrivateKey'));
-    res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']));
-}));
+      },
+      config.get("jwtPrivateKey")
+    );
+    res
+      .header("x-auth-token", token)
+      .send(_.pick(user, ["_id", "name", "email"]));
+  })
+);
 
-router.get("/me", auth, asyncMiddleware(async (req, res) => {
-    const user = await User.findById(req.user._id).select('-password');
-    if (!user) return res.status(400).send('User not exist.');
+router.get(
+  "/me",
+  auth,
+  asyncMiddleware(async (req, res) => {
+    const user = await Schemas.User.findById(req.user._id).select("-password");
+    if (!user) return res.status(400).send("User not exist.");
     res.send(user);
-}));
+  })
+);
 
-router.put("/me/password", auth, asyncMiddleware(async (req, res) => {
-    const {
-        error
-    } = validate.validatePassword(req.body);
+router.get("/:id", auth, async (req, res) => {
+  const user = await Schemas.User.findById(req.params.id).select("-password");
+  if (!user) return res.status(400).send("User not exist.");
+  res.send(user);
+});
+
+router.put(
+  "/me/password",
+  auth,
+  asyncMiddleware(async (req, res) => {
+    const { error } = validate.validatePassword(req.body);
     if (error) return res.status(400).send(error.details[0].message);
-    await Schemas.User.updateOne({
+    await Schemas.User.updateOne(
+      {
         nick: req.params.nick
-    }, {
+      },
+      {
         $set: {
-            password: req.body.password
+          password: req.body.password
         }
-    });
+      }
+    );
     res.send("Password has been changed");
-}));
+  })
+);
 
-router.put("/me/number", auth, asyncMiddleware(async (req, res) => {
-    let number = await Schemas.User.update({
+router.put(
+  "/me/number",
+  auth,
+  asyncMiddleware(async (req, res) => {
+    let number = await Schemas.User.update(
+      {
         _id: req.body.id
-    }, {
+      },
+      {
         $inc: {
-            last: 1
+          last: 1
         }
-    });
+      }
+    );
     res.send(number);
-}));
+  })
+);
 
-router.delete("/:nick", auth, asyncMiddleware(async (req, res) => {
+router.delete(
+  "/:nick",
+  auth,
+  asyncMiddleware(async (req, res) => {
     const user = await Schemas.User.deleteOne({
-        nick: req.params.nick
+      nick: req.params.nick
     });
-    if (!user) return res.status(400).send('User not exist.');
+    if (!user) return res.status(400).send("User not exist.");
     res.send(user);
-}));
+  })
+);
 
 module.exports = router;
